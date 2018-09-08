0bb45ace3db16b29cfc33e2056ea3bf20cbd4b5a
Pandalove102 <phamminhphuc102@gmail.com>
2018-09-08 23:11:15 +0700

    add lần 1
    add lần 1

---
+++ view/user3/search.php
@@ -0,0 +1,68 @@
+<section class="page-content">
+			<!-- 404-AREA START -->
+			<div class="area-404 margin-bottom-80">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="error">
+								<div class="error-title">
+									<h1>Search</h1>
+								</div>
+								<!-- <div class="eror-brief">
+									<h2>This page is not available</h2>
+									<h3>We're sorry, but the Web address you've entered is no longer available.</h3>
+								</div> -->
+								<div class="eror-form">
+									<form action="?controller=user3&act=search" method="POST">
+										<span>Search our product catalog :</span>
+										<input type="text" name="kw" class="form-control">
+										<button name="search" value="1" type="submit">Search</button>
+                                    </form>
+                                  
+								</div>
+								<div class="back-home">
+									<a href="?controller=user3&act=trangchu"> home page</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- 404-AREA END -->
+			<!-- BRAND-LOGO-AREA START -->
+			<!-- <div class="brand-logo-area margin-bottom-80">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-5 col-sm-12">
+							<div class="brand-brief">
+								<h2 class="border-left-right">they are with us</h2>
+								<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
+							</div>
+						</div>
+						<div class="col-md-7 col-sm-12">
+							<div class="brand-logo fix">
+								<div class="single-logo">
+									<img src="img/brand/1.png" alt="" />
+								</div>
+								<div class="single-logo">
+									<img src="img/brand/2.png" alt="" />
+								</div>
+								<div class="single-logo">
+									<img src="img/brand/3.png" alt="" />
+								</div>
+								<div class="single-logo">
+									<img src="img/brand/4.png" alt="" />
+								</div>
+								<div class="single-logo">
+									<img src="img/brand/5.png" alt="" />
+								</div>
+								<div class="single-logo">
+									<img src="img/brand/6.png" alt="" />
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div> -->
+			<!-- BRAND-LOGO-AREA END -->
+		</section>
